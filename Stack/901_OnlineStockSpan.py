a4c348344909f63ddc46aa2f80b028cd607a50fc
Yassine Zakhama <yzakhama@gmail.com>
2025-01-18 18:43:05 +0100

    leetcode 901; remove unnecessary array

--- Stack/901_OnlineStockSpan.py
+++ Stack/901_OnlineStockSpan.py
@@ -1,19 +1,10 @@
 class StockSpanner:
     def __init__(self):
-        self.prices = []
-        self.stack = [] # (p, i) tuples        
+        self.stack = [] # (price, span) tuples        
 
     def next(self, price: int) -> int:
-        self.prices.append(price)
-        i = len(self.prices) - 1
-
-        if not self.stack or self.stack[-1][0] > price:
-            self.stack.append((price, i))
-            return 1
-        
+        span = 1
         while self.stack and self.stack[-1][0] <= price:
-            self.stack.pop() 
-        res = i + 1 if not self.stack else i - self.stack[-1][1]
-        self.stack.append((price, i))
-        return res 
-        
+            span += self.stack.pop()[1]
+        self.stack.append((price, span))
+        return span
