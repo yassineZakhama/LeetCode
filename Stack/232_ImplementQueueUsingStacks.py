c269e7e6c32f59407865646b1de36c0346d30bc4
Yassine Zakhama <yzakhama@gmail.com>
2025-01-18 12:08:17 +0100

    leetcode 232: amortized O(1) all operations

--- Stack/232_ImplementQueueUsingStacks.py
+++ Stack/232_ImplementQueueUsingStacks.py
@@ -1,28 +1,24 @@
 class MyQueue:
     def __init__(self):
-        self.stack = []
-        self.temp = []
-        self.top = 0
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
-        if not len(self.stack):
-            self.top = x
-        self.stack.append(x)
+        self.stack1.append(x)
 
     def pop(self) -> int:
-        if len(self.stack) == 1:
-            return self.stack.pop()
-        
-        while len(self.stack) != 1:
-            self.temp.append(self.stack.pop())
-        self.top = self.temp[-1]
-        res = self.stack.pop()
-        while self.temp:
-            self.stack.append(self.temp.pop())
-        return res
+        if len(self.stack2):
+            return self.stack2.pop()        
+        while len(self.stack1) != 1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack1.pop()
 
     def peek(self) -> int:
-        return self.top
+        if len(self.stack2):
+            return self.stack2[-1]        
+        while len(self.stack1):
+            self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
 
     def empty(self) -> bool:
-        return len(self.stack) == 0
+        return len(self.stack1) == 0 and len(self.stack2) == 0
