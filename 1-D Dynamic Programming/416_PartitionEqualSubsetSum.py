62bcf3ff7351f9c4d58d0ab4c2157afdf0db0a9c
Yassine Zakhama <yzakhama@gmail.com>
2025-01-22 22:38:28 +0100

    leetcode 416: code cleaning

--- 1-D Dynamic Programming/416_PartitionEqualSubsetSum.py
+++ 1-D Dynamic Programming/416_PartitionEqualSubsetSum.py
@@ -6,19 +6,20 @@ class Solution:
         if arrSum % 2 != 0:
             return False
 
+        TARGET = arrSum // 2
         memo = {}
-
-        def dfs(i, currSum, target):
-            if currSum > target or i >= len(nums):
+        
+        def dfs(i, currSum):
+            if currSum > TARGET or i >= len(nums):
                 return False
             
             key = (i, currSum) 
             if key in memo:
                 return memo[key]            
-            if currSum == target:
+            if currSum == TARGET:
                 return True
             
-            memo[key] = dfs(i+1, currSum  + nums[i], target) or dfs(i+1, currSum, target)
+            memo[key] = dfs(i+1, currSum  + nums[i]) or dfs(i+1, currSum)
             return memo[key] 
 
-        return dfs(0, 0, arrSum / 2)
+        return dfs(0, 0)
