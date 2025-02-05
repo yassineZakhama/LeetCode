62c43e2dfd05a1e612839961592a6a5c1945772f
Yassine Zakhama <yzakhama@gmail.com>
2025-02-05 16:39:10 +0100

    1790: lengths are always equal

--- Random/1790_CheckIfOneStringSwapCanMakeStringsEqual.py
+++ Random/1790_CheckIfOneStringSwapCanMakeStringsEqual.py
@@ -1,8 +1,5 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2):
-            return False
-        
         differences = []
         for i in range(len(s1)):
             if s1[i] != s2[i]:
