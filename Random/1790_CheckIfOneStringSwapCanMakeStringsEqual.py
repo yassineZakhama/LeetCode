5cd7d119b6c779d12a2a2586ebbb01bcd3d3b23d
Yassine Zakhama <yzakhama@gmail.com>
2025-02-05 16:36:57 +0100

    1790: daily challenge

---
+++ Random/1790_CheckIfOneStringSwapCanMakeStringsEqual.py
@@ -0,0 +1,14 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        
+        differences = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                if len(differences) == 2:
+                    return False
+                differences.append(i)
+        
+        return (not differences or len(differences) == 2 
+            and s1[differences[0]] == s2[differences[1]] and s2[differences[0]] == s1[differences[1]])
