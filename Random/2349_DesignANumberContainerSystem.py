f51cadf02a499c91409e43b2febde0fa5fc7675e
Yassine Zakhama <yzakhama@gmail.com>
2025-02-08 18:50:49 +0100

    2349 - daily challenge: minheap solution with lazy deletion

--- Random/2349_DesignANumberContainerSystem.py
+++ Random/2349_DesignANumberContainerSystem.py
@@ -1,18 +1,21 @@
 from collections import defaultdict
+import heapq
 
 class NumberContainers:
 
     def __init__(self):
-        self.numberToIndices = defaultdict(SortedSet)
+        self.numberToIndices = defaultdict(list)
         self.indexToNumber = {}
 
     def change(self, index: int, number: int) -> None:
-        if index in self.indexToNumber:
-            oldNumber = self.indexToNumber[index]
-            self.numberToIndices[oldNumber].remove(index)
-
         self.indexToNumber[index] = number
-        self.numberToIndices[number].add(index)
+        heapq.heappush(self.numberToIndices[number], index)
 
     def find(self, number: int) -> int:
-        return self.numberToIndices[number][0] if self.numberToIndices[number] else -1
+        while self.numberToIndices[number]:
+            currMin = self.numberToIndices[number][0]
+            if number == self.indexToNumber[currMin]:
+                return currMin
+            heapq.heappop(self.numberToIndices[number])
+
+        return -1
