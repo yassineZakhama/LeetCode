a1326d90dcdb0b097636e506ef90e589dcb5b938
Yassine Zakhama <yzakhama@gmail.com>
2025-02-11 22:29:53 +0100

    52 - optimal

--- NeetCode-250/Backtracking/51_N-Queens.py
+++ NeetCode-250/Backtracking/51_N-Queens.py
@@ -1,42 +1,30 @@
 from typing import List
 
-def isSafe(r, c, board):
-    row = r - 1
-    while row >= 0:
-        if board[row][c] == "Q":
-            return False
-        row -= 1
-        
-    row, col = r - 1, c - 1
-    while row >= 0 and col >= 0:
-        if board[row][col] == "Q":
-            return False
-        row -= 1
-        col -= 1
-
-    row, col = r - 1, c + 1
-    while row >= 0 and col < len(board):
-        if board[row][col] == "Q":
-            return False
-        row -= 1
-        col += 1
-    return True
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [["."] * n for _ in range(n)]
+        cols, negDiag, posDiag = set(), set(), set()
         res = []
 
-        def dfs(i):
-            if i == n:
+        def backtrack(r):
+            if r == n:
                 res.append(["".join(row) for row in board])
                 return
 
-            for j in range(n):
-                if isSafe(i, j, board):
-                    board[i][j] = "Q"
-                    dfs(i+1)
-                    board[i][j] = "."
+            for c in range(n):
+                neg, pos = r - c, r + c
+                if c not in cols and neg not in negDiag and pos not in posDiag:
+                    cols.add(c)
+                    negDiag.add(neg)
+                    posDiag.add(pos)
+                    board[r][c] = "Q"
 
-        dfs(0)
+                    backtrack(r+1)
+                    
+                    board[r][c] = "."
+                    cols.remove(c)
+                    negDiag.remove(neg)
+                    posDiag.remove(pos)
+
+        backtrack(0)
         return res
