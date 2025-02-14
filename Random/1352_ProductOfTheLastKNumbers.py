d03c815c78bfa6262023446743028211ca9cddf5
Yassine Zakhama <yzakhama@gmail.com>
2025-02-14 19:22:26 +0100

    1352 - daily challenge

--- Random/1352_ProductOfTheLastKNumbers.py
+++ Random/1352_ProductOfTheLastKNumbers.py
@@ -1,27 +1,16 @@
 class ProductOfNumbers:
 
     def __init__(self):
-        self.lastZero = -1
-        self.product = []
+        self.prefixProducts = [1]
 
     def add(self, num: int) -> None:
         if num == 0:
-            self.lastZero = len(self.product)
-            self.product.append(0)
-        elif not self.product or self.product[-1] == 0:
-            self.product.append(num)
+            self.prefixProducts = [1]
         else:
-            self.product.append(num * self.product[-1])
+            self.prefixProducts.append(self.prefixProducts[-1] * num)
 
     def getProduct(self, k: int) -> int:
-        startInterval = len(self.product) - k
-        if startInterval == 0:
-            return self.product[-1] if self.lastZero == -1 else 0
-
-        if self.lastZero >= startInterval:
+        startInterval = len(self.prefixProducts) - k
+        if startInterval < 1:
             return 0
-
-        if self.product[startInterval - 1] != 0:
-            return self.product[-1] // self.product[startInterval - 1]
-        else:
-            return self.product[-1]
+        return self.prefixProducts[-1] // self.prefixProducts[startInterval - 1]
