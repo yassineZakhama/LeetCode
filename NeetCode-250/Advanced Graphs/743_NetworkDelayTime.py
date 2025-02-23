2dd250175ce290670c9136d24aff680ea72abafb
Yassine Zakhama <yzakhama@gmail.com>
2025-02-23 16:35:18 +0100

    743

---
+++ NeetCode-250/Advanced Graphs/743_NetworkDelayTime.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import heapq
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+        
+        vis, heap = set(), []
+        dist = [float("inf")] * (n + 1)
+        dist[0], dist[k] = 0, 0
+
+        for v, w in adj[k]:
+            dist[v] = w
+            heapq.heappush(heap, (w, v))
+
+        while heap:
+            d, u = heapq.heappop(heap)
+            for v, w in adj[u]:
+                if v not in vis and d + w < dist[v]:
+                    dist[v] = d + w
+                    heapq.heappush(heap, (d + w, v))
+            vis.add(u)
+
+        res = max(dist)
+        return res if res != float("inf") else -1
